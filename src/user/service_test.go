f5c218d3b10f91af4184310e5c462f9ca822c650
Amir Parsa <develop.apm@gmail.com>
2023-10-30 08:28:18 +0330

    Feature: add GetUserByID unit tests

--- src/user/service_test.go
+++ src/user/service_test.go
@@ -22,69 +22,125 @@ func Test_AddNewUser(t *testing.T) {
 	}
 	testCases := []struct {
 		desc         string
-		inputs       *Inputs
+		input        *Inputs
 		prepareMocks func(m *Mocks)
-		expectResult *domain.User
-		expectErr    error
+		expectedUser *domain.User
+		expectedErr  error
 	}{
 		{
-			desc: "when receives invalid name with length < 3, should return error",
-			inputs: &Inputs{
+			desc: "Invalid name with length < 3",
+			input: &Inputs{
 				name: []string{"", "a", "ab"}[rand.Intn(3)],
 			},
 			prepareMocks: func(m *Mocks) {},
-			expectResult: nil,
-			expectErr:    domain.ErrInvalidArgument,
+			expectedUser: nil,
+			expectedErr:  domain.ErrInvalidArgument,
 		},
 		{
-			desc: "when fails to persist user to DB, should return error",
-			inputs: &Inputs{
+			desc: "Internal server error",
+			input: &Inputs{
 				name: "Amir",
 			},
 			prepareMocks: func(m *Mocks) {
 				m.ur.On("Insert", mock.Anything, mock.Anything).
 					Return(errors.New("some db error")).Once()
 			},
-			expectResult: nil,
-			expectErr:    domain.ErrInternalServer,
+			expectedUser: nil,
+			expectedErr:  domain.ErrInternalServer,
 		},
 		{
-			desc: "when creates and persists user to DB, should return user",
-			inputs: &Inputs{
+			desc: "Create user",
+			input: &Inputs{
 				name: "Parsa",
 			},
 			prepareMocks: func(m *Mocks) {
 				m.ur.On("Insert", mock.Anything, mock.Anything).
 					Return(nil).Once()
 			},
-			expectResult: &domain.User{
+			expectedUser: &domain.User{
 				Name: "Parsa",
 			},
-			expectErr: nil,
+			expectedErr: nil,
 		},
 	}
 
 	for _, tc := range testCases {
-		// Arrange
-		mocks := &Mocks{
-			ur: mocks.NewUserRepository(t),
-		}
-		tc.prepareMocks(mocks)
-		svc := user.NewService(mocks.ur)
+		t.Run(tc.desc, func(t *testing.T) {
+			// Arrange
+			mocks := &Mocks{
+				ur: mocks.NewUserRepository(t),
+			}
+			tc.prepareMocks(mocks)
+			svc := user.NewService(mocks.ur)
+			// Action
+			user, err := svc.AddNewUser(context.Background(), tc.input.name)
+			// Assert
+			if tc.expectedUser != nil {
+				assert.NotEmpty(t, user.ID, tc.desc)
+				assert.Equal(t, tc.expectedUser.Name, user.Name, tc.desc)
+			}
+			if tc.expectedErr != nil {
+				assert.ErrorIs(t, err, tc.expectedErr, tc.desc)
+			} else {
+				assert.NoError(t, err, tc.desc)
+			}
+			mocks.ur.AssertExpectations(t)
+		})
+	}
+}
 
-		// Action
-		user, err := svc.AddNewUser(context.Background(), tc.inputs.name)
+func Test_GetUserByID(t *testing.T) {
+	testCases := []struct {
+		desc         string
+		userID       string
+		repoResponse *domain.User
+		repoErr      error
+		expectedUser *domain.User
+		expectedErr  error
+	}{
+		{
+			desc:         "User found",
+			userID:       "abc",
+			repoResponse: &domain.User{ID: "abc", Name: "Amir"},
+			repoErr:      nil,
+			expectedUser: &domain.User{ID: "abc", Name: "Amir"},
+			expectedErr:  nil,
+		},
+		{
+			desc:         "User not found",
+			userID:       "def",
+			repoResponse: nil,
+			repoErr:      nil,
+			expectedUser: nil,
+			expectedErr:  domain.ErrNotFound,
+		},
+		{
+			desc:         "Internal server error",
+			userID:       "ghi",
+			repoResponse: nil,
+			repoErr:      errors.New("some error"),
+			expectedUser: nil,
+			expectedErr:  domain.ErrInternalServer,
+		},
+	}
 
-		// Assert
-		if tc.expectErr != nil {
-			assert.ErrorIs(t, err, tc.expectErr, tc.desc)
-		} else {
-			assert.NoError(t, err, tc.desc)
-		}
-		if tc.expectResult != nil {
-			assert.NotEmpty(t, user.ID, tc.desc)
-			assert.Equal(t, tc.expectResult.Name, user.Name, tc.desc)
-		}
-		mocks.ur.AssertExpectations(t)
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			// Arrange
+			repo := mocks.NewUserRepository(t)
+			repo.On("FindByID", mock.Anything, tc.userID).Return(tc.repoResponse, tc.repoErr)
+
+			s := user.NewService(repo)
+			// Action
+			user, err := s.GetUserByID(context.Background(), tc.userID)
+			// Assert
+			assert.Equal(t, tc.expectedUser, user, tc.desc)
+			if tc.expectedErr != nil {
+				assert.ErrorIs(t, err, tc.expectedErr, tc.desc)
+			} else {
+				assert.NoError(t, err, tc.desc)
+			}
+			repo.AssertExpectations(t)
+		})
 	}
 }
