8a9d314f7e1218c24b9a4bcc3ebb96b403f6f332
Amir Parsa <develop.apm@gmail.com>
2023-10-30 12:49:31 +0330

    Refactor: user entity unit tests

--- src/domain/user_test.go
+++ src/domain/user_test.go
@@ -8,63 +8,64 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_User_IsNew(t *testing.T) {
+func Test_NewUser(t *testing.T) {
 	testCases := []struct {
-		desc   string
-		user   *domain.User
-		expect bool
+		desc          string
+		inputName     string
+		expectedUser  *domain.User
+		expectedError error
 	}{
 		{
-			desc: "when user has ID, should return false",
-			user: &domain.User{
-				ID:   uuid.New().String(),
-				Name: "Amir",
-			},
-			expect: false,
+			desc:          "Valid user",
+			inputName:     "Amir",
+			expectedUser:  &domain.User{Name: "Amir"},
+			expectedError: nil,
 		},
 		{
-			desc: "when user does not have ID, should return true",
-			user: &domain.User{
-				ID:   "",
-				Name: "Amir",
-			},
-			expect: true,
+			desc:          "Invalid name",
+			inputName:     "A",
+			expectedUser:  nil,
+			expectedError: domain.ErrInvalidArgument,
 		},
 	}
 
 	for _, tc := range testCases {
-		assert.Equal(t, tc.expect, tc.user.IsNew(), tc.desc)
+		t.Run(tc.desc, func(t *testing.T) {
+			user, err := domain.NewUser(tc.inputName)
+
+			if tc.expectedUser != nil {
+				assert.NotEmpty(t, user.ID, tc.desc)
+				assert.Equal(t, tc.expectedUser.Name, user.Name, tc.desc)
+			}
+			if tc.expectedError != nil {
+				assert.ErrorIs(t, err, tc.expectedError, tc.desc)
+			} else {
+				assert.NoError(t, err, tc.desc)
+			}
+		})
 	}
 }
 
-func Test_User_TryGenerateID(t *testing.T) {
-	testCases := []struct {
-		desc   string
-		user   *domain.User
-		expect bool
-	}{
-		{
-			desc: "when user has ID, should return false",
-			user: &domain.User{
-				ID:   uuid.New().String(),
-				Name: "Amir",
-			},
-			expect: false,
-		},
-		{
-			desc: "when user does not have ID, should fill the ID and return true",
-			user: &domain.User{
-				ID:   "",
-				Name: "Amir",
-			},
-			expect: true,
-		},
-	}
+func Test_IsNew(t *testing.T) {
+	user := &domain.User{}
+	assert.True(t, user.IsNew())
 
-	for _, tc := range testCases {
-		assert.Equal(t, tc.expect, tc.user.TryGenerateID(), tc.desc)
-		if tc.expect == true {
-			assert.NotEmpty(t, tc.user.ID, tc.desc)
-		}
-	}
+	user.ID = "123-abc-456-def"
+	assert.False(t, user.IsNew())
+}
+
+func Test_TryGenerateID(t *testing.T) {
+	user := &domain.User{}
+	generated := user.TryGenerateID()
+
+	assert.True(t, generated)
+	assert.NotEmpty(t, user.ID)
+	_, err := uuid.Parse(user.ID)
+	assert.NoError(t, err)
+
+	// ID already exists, should not generate a new one
+	previousID := user.ID
+	generated = user.TryGenerateID()
+	assert.False(t, generated)
+	assert.Equal(t, previousID, user.ID)
 }
